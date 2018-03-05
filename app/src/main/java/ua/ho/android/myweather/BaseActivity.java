a52aa7ad611e9e66de9762a502f6f6076cefd3d9
piter <vol.peterburg@gmail.com>
2018-03-05 21:24:33 +0200

    test first1

--- app/src/main/java/ua/ho/android/myweather/BaseActivity.java
+++ app/src/main/java/ua/ho/android/myweather/BaseActivity.java
@@ -3,10 +3,16 @@ package ua.ho.android.myweather;
 
 import android.app.Activity;
 import android.content.Context;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
 
 
 public class BaseActivity extends Activity {
     public Context mContext;
 
 
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
 }
